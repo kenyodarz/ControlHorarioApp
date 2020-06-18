0e60799e0adaff9175949af749435d8be33465ac
Kenyodarz <minajorge@hotmail.com>
2020-06-18 07:29:35 -0500

    Initial App Components Version 0.0.0.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { InformeComponent } from './components/informe/informe.component';
+import { LoginComponent } from './components/login/login.component';
+import { PersonaComponent } from './components/persona/persona.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PrimengModule } from './primeng.module';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    InformeComponent,
+    LoginComponent,
+    PersonaComponent,
+    ProfileComponent,
+    ProyectoComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    PrimengModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
